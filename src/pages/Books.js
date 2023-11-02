57a1f718ec91d51107389aac3a636915491ad37c
Mosky117 <davi.mosky@outlook.com>
2023-11-02 18:17:18 +0100

    .

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -2,13 +2,12 @@ import React from "react";
 import '../css/Books.css'
 import '../css/Home.css'
 import Footer from "../components/Footer";
-import BookReviews from "../components/BookLists";
+import BookLists from "../components/BookLists";
 
 const Books = () => (
   <div className="page">
-    {/* <BestSellers/> */}
-    <h1>Book Reviews</h1>
-    <BookReviews/>
+    <h1>Recommended Books</h1>
+    <BookLists/>
     <a href='/' className='section title'><h3>New York Times</h3></a>
     <Footer/>
   </div>
