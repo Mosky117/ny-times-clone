12c5651a118e05f4f35acbefe1ef18a046cd787a
Mosky117 <davi.mosky@outlook.com>
2023-11-16 19:25:27 +0100

    loader

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import Navbar from './components/Navbar';
 import Home from './routes/Home';
 import Books from './routes/Books';
 import { Route, Routes } from 'react-router-dom';
+import React, { useEffect, useState  } from 'react';
 
 function App() {
+  const [loading, setLoading]= useState(true);
+  const spinner= document.getElementById('spinner');
+  if(spinner){
+    setTimeout(()=>{
+      spinner.style.display='none';
+      setLoading(false);
+    },2000);
+  }
+
   return (
-    <>
-      <Navbar />
-      <div>
-        <Routes basename={process.env.PUBLIC_URL}>
-          <Route path='/' element={<Home/>}/>
-          
-          <Route path='/Books' element={<Books/>}/>
-        </Routes>
-      </div>
-    </>
+    !loading &&(
+      <>
+        <Navbar />
+        <div>
+          <Routes basename={process.env.PUBLIC_URL}>
+            <Route path='/' element={<Home/>}/>
+            
+            <Route path='/Books' element={<Books/>}/>
+          </Routes>
+        </div>
+      </>
+    )
   );
 }
 
